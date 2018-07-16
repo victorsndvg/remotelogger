63e5d34ce9fbf7bde8ec026845f23bdac2a5d079
victorsndvg <victorsv@gmail.com>
2018-07-16 22:11:26 +0200

    first steps and basic features to explore logs

--- code/logconsole/views.py
+++ code/logconsole/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render_to_response
 from django.shortcuts import render
 from django.template import Context, Template
 from django.http import HttpResponse
-from remotelogger.mongo import Log
+from remotelogger.mongo import Log, StorageInfo
 
 logger = logging.getLogger(__name__)
 
@@ -14,8 +14,23 @@ basedir = os.path.dirname(os.path.realpath(__file__))
 def index(request):
     return HttpResponse(open(os.path.join(basedir, 'templates/index.html')))
 
-def logs(request, user, room, namespace):
-    log = Log(exchange=user, routing_key=room, queue=namespace, logger=logger)
+def users(request):
+    users = StorageInfo(logger).list_databases()
+    context = {"USERS": users}
+    return render(request, 'users.html', context)
+
+def workflows(request, exchange):
+    workflows = StorageInfo(logger).list_collections(exchange)
+    context = {"WORKFLOWS": workflows}
+    return render(request, 'workflows.html', context)
+
+def jobs(request, exchange, routing_key):
+    jobs = StorageInfo(logger).list_documents(exchange, routing_key)
+    context = {"JOBS": jobs}
+    return render(request, 'jobs.html', context)
+
+def logs(request, exchange, routing_key, queue):
+    log = Log(exchange=exchange, routing_key=routing_key, queue=queue, logger=logger)
     history = log.get()
-    context = {"USER": user, "ROOM": room, "NAMESPACE": namespace, "HISTORY": history}
-    return render(request, 'index.html', context)
+    context = {"USER": exchange, "ROOM": routing_key, "NAMESPACE": queue, "HISTORY": history}
+    return render(request, 'logs.html', context)
--- code/remotelogger/mongo.py
+++ code/remotelogger/mongo.py
@@ -8,7 +8,6 @@ from remotelogger.settings import STORAGE_URL
 class Log(object):
 
     def __init__(self, exchange, queue, routing_key, logger):
-        self._id            = None
         self._info          = {"exchange":exchange, "queue":queue, "routing_key": routing_key}
         self._client        = MongoClient(STORAGE_URL)
         self._collection    = self._client[exchange][routing_key]
@@ -28,4 +27,26 @@ class Log(object):
     def get(self):
         log = self._collection.find_one(self._info)
         return log
+
+class StorageInfo(object):
+
+    def __init__(self, logger):
+        self._client = MongoClient(STORAGE_URL)
+        self.logger  = logger
+
+    def list_databases(self):
+        self.logger.info('List databases')
+        return [db for db in self._client.database_names() if db not in  ['admin', 'config', 'local']]
+
+    def list_collections(self, database):
+        self.logger.info('List collections in database: "%s"', database)
+        db = self._client[database]
+        return db.collection_names()
+
+    def list_documents(self, database, collection):
+        self.logger.info('List documents in database: "%s", collection: "%s"', database, collection)
+        db = self._client[database]
+        collection = db[collection]
+        return [doc['queue'] for doc in collection.find()]
+
 
